46b82faf1e9f2482c5f654e8e3554de0963d2afe
laichunx <48215216+laichunx@users.noreply.github.com>
2021-03-14 19:29:41 +0800

    added cards css

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import React from "react";
 import SearchMovie from "./searchMovies";
 
+
 function App() {
   return (
     <div className="container">
-      <h1 className="title"> React Movie Search </h1>
+      <h1 className="title mb-5"> Movie Search </h1>
       <SearchMovie/>
     </div>
   );
--- movie-search/src/searchMovies.js
+++ movie-search/src/searchMovies.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
-import CustomCard from "./components/CustomCard"
+import Card from "react-bootstrap/Card";
+import CardColumns from "react-bootstrap/CardColumns";
 
 export default function SearchMovies(){
 
@@ -8,29 +9,43 @@ export default function SearchMovies(){
 
     const search = async (e) => {
         e.preventDefault();
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=916671b5d10c6031808c69f06c770e69&language=en-US&query=${query}&page=1&include_adult=false`;
-        try{
-            const res = await fetch(url);
-            const data = await res.json();
-            setMovies(data.results);
-        }catch(err){
-            console.error(err);
+        if(query!==''){
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=916671b5d10c6031808c69f06c770e69&language=en-US&query=${query}&page=1&include_adult=false`;
+            try{
+                const res = await fetch(url);
+                const data = await res.json();
+                setMovies(data.results);
+            }catch(err){
+                console.error(err);
+            }
         }
     }
 
     return(
         <div>
-            <form className="form" onSubmit={search}>
-                <label className="label" htmlFor="query" >Movie Name</label>
+            <form className="form mb-5" onSubmit={search}>
                 <input className="input" type="text" name="query" placeholder="Alice in Wonderland"
                     value={query} onChange={(e) => setQuery(e.target.value)}></input>
                 <button className="button" type="submit">Search</button>
             </form>
-            <div className="card-list">
+            <CardColumns>
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <CustomCard movie={movie} key={movie.id}/>
+                        <Card className="mb-2 w-75" key={movie.id}>
+                            <Card.Img
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt={"movie.title" + " poster"}
+                            />
+                            <Card.Body>
+                                <Card.Title>
+                                    <h3>{movie.title}</h3>
+                                </Card.Title>
+                                <Card.Text>
+                                    <p>{movie.overview}</p>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                 ))}
-            </div>
+            </CardColumns>
         </div>
     )
 }
